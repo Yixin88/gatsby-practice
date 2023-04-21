319f05edfd01133ea453642e42b7be4fa6333e8d
Yixin <yixin88@hotmail.co.uk>
2023-04-21 15:43:09 +0100

    added dynamic md content to the blog pages

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
 
-export default function Blog() {
+
+export const query = graphql`
+  query (
+    $slug: String!
+  ) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+export default function Blog(props) {
   return (
     <Layout>
-      This is the blog template
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}></div>
     </Layout>
   )
 }
