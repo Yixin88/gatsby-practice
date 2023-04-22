0ac461e0088c8497d4569ff549fc29aa8ecc9e7c
Yixin <yixin88@hotmail.co.uk>
2023-04-22 16:12:44 +0100

    blog pages are now showing the title and date with contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -3,32 +3,40 @@ import Layout from '../components/Layout'
 import { graphql } from 'gatsby'
 
 
+// export const query = graphql`
+//   query (
+//     $slug: String!
+//   ) {
+//     markdownRemark (
+//       fields: {
+//         slug: {
+//           eq: $slug
+//         }
+//       }
+//     ) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 export const query = graphql`
-  query (
-    $slug: String!
-  ) {
-    markdownRemark (
-      fields: {
-        slug: {
-          eq: $slug
-        }
-      }
-    ) {
-      frontmatter {
-        title
-        date
-      }
-      html
+  query($slug: String!) {
+    contentfulBlogPost(slug: {eq : $slug}) {
+      title
+      publishedDate(formatString: "Do MMMM, YYYY")
     }
   }
 `
 
+
 export default function Blog(props) {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}}></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
     </Layout>
   )
 }
